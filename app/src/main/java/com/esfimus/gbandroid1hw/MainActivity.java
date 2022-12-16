ec20ebe064c21592ac4657b81f549233c1198edc
Nikolay N. Nikolaev <esfimus@gmail.com>
2022-12-16 21:32:40 +0300

    новая активность с макетом по заданию и переход с главной

--- app/src/main/java/com/esfimus/gbandroid1hw/MainActivity.java
+++ app/src/main/java/com/esfimus/gbandroid1hw/MainActivity.java
@@ -1,6 +1,7 @@
 package com.esfimus.gbandroid1hw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -19,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
         String destinationText = destination.getText().toString();
         TextView messageToShow = findViewById(R.id.textMessage);
         messageToShow.setText(destinationText);
+    }
+
+    public void nextActivity(View view) {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
