7c9bc1af9bcf27ee491a373ae316f5234a2ecfdf
Nikolay N. Nikolaev <esfimus@gmail.com>
2022-12-22 21:06:55 +0300

    Реализация Parcelable для сохранения и восстановления данных.

--- app/src/main/java/com/esfimus/gbandroid1hw/Presenter.java
+++ app/src/main/java/com/esfimus/gbandroid1hw/Presenter.java
@@ -1,7 +1,17 @@
 package com.esfimus.gbandroid1hw;
 
-public class Presenter {
-    private String input = "";
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Presenter implements Parcelable {
+
+    private String input;
+
+    public Presenter() {
+        this.input = "";
+    }
 
     public String getResult() {
         return input;
@@ -13,5 +23,31 @@ public class Presenter {
 
     public void passValue(String value) {
         input += value;
+    }
+
+    protected Presenter(Parcel in) {
+        input = in.readString();
+    }
+
+    public static final Creator<Presenter> CREATOR = new Creator<Presenter>() {
+        @Override
+        public Presenter createFromParcel(Parcel in) {
+            return new Presenter(in);
+        }
+
+        @Override
+        public Presenter[] newArray(int size) {
+            return new Presenter[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(input);
     }
 }
