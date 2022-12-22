664e39f257da3238ffd06c24ba1c4fdce9f1abd1
Nikolay N. Nikolaev <esfimus@gmail.com>
2022-12-22 18:40:18 +0300

    Обработка всех кнопок. Лаконичный код с дополнительным методом для всех кнопок.

--- app/src/main/java/com/esfimus/gbandroid1hw/CalculatorActivity.java
+++ app/src/main/java/com/esfimus/gbandroid1hw/CalculatorActivity.java
@@ -2,6 +2,7 @@ package com.esfimus.gbandroid1hw;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,20 +25,30 @@ public class CalculatorActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
         auxResult = findViewById(R.id.subresult);
 
-        findViewById(R.id.button_0).setOnClickListener(v -> {
-            presenter.passValue("0");
-            result.setText(presenter.getResult());
-            auxResult.setText(presenter.getSubResult());
-        });
+        buttonClickListener(findViewById(R.id.button_0), "0");
+        buttonClickListener(findViewById(R.id.button_1), "1");
+        buttonClickListener(findViewById(R.id.button_2), "2");
+        buttonClickListener(findViewById(R.id.button_3), "3");
+        buttonClickListener(findViewById(R.id.button_4), "4");
+        buttonClickListener(findViewById(R.id.button_5), "5");
+        buttonClickListener(findViewById(R.id.button_6), "6");
+        buttonClickListener(findViewById(R.id.button_7), "7");
+        buttonClickListener(findViewById(R.id.button_8), "8");
+        buttonClickListener(findViewById(R.id.button_9), "9");
+        buttonClickListener(findViewById(R.id.button_plus), "+");
+        buttonClickListener(findViewById(R.id.button_minus), "-");
+        buttonClickListener(findViewById(R.id.button_multi), "*");
+        buttonClickListener(findViewById(R.id.button_div), "/");
+        buttonClickListener(findViewById(R.id.button_percent), "%");
+        buttonClickListener(findViewById(R.id.button_dot), ".");
+        buttonClickListener(findViewById(R.id.button_eq), "=");
+        buttonClickListener(findViewById(R.id.button_del), "del");
+        buttonClickListener(findViewById(R.id.button_ac), "clear");
+    }
 
-        findViewById(R.id.button_1).setOnClickListener(v -> {
-            presenter.passValue("1");
-            result.setText(presenter.getResult());
-            auxResult.setText(presenter.getSubResult());
-        });
-
-        findViewById(R.id.button_2).setOnClickListener(v -> {
-            presenter.passValue("2");
+    private void buttonClickListener(Button button, String value) {
+        button.setOnClickListener(v -> {
+            presenter.passValue(value);
             result.setText(presenter.getResult());
             auxResult.setText(presenter.getSubResult());
         });
